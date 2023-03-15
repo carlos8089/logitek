1e4ac9e68cfaba12da05024f5f840dd72450c13b
Carlos SAH JOHNSON <45031770+carlos8089@users.noreply.github.com>
2023-03-15 12:42:52 +0300

    Delete logitek.sql

--- logitek.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT into categories(name) VALUES('Education');
-INSERT into categories(name) VALUES('Entertainment');
-INSERT into categories(name) VALUES('Games');
-INSERT into categories(name) VALUES('Health Care');
-INSERT into categories(name) VALUES('Productivity');
